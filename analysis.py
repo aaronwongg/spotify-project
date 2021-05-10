2055fb892daae5ab39f9df6c9c5367942dd2d541
Aaron Wong <aaronw@aarons-mbp.myfiosgateway.com>
2021-05-10 14:49:07 -0400

    Starting Code

---
+++ analysis.py
@@ -0,0 +1,13 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = 'user-library-read'
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+
+for idx, item in enumrate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], ' - ', track['name'])
+    
